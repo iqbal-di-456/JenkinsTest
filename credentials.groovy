e30e9c59fe0929f77916130eb78030ba4641f98b
Md Iqbal <miqbal@datainnovations.com>
2023-11-16 23:28:33 +0530

    Change series 18

--- credentials.groovy
+++ credentials.groovy
@@ -8,18 +8,18 @@ return {
     println "Current Branch: ${env.BRANCH_NAME}"
     
     // Conditional check using 'if' statement
-    if (BRANCH_NAME == 'dev') {
+    if (env.BRANCH_NAME == 'dev') {
+        GIT_CREDENTIALS_ID = 'c17f17fc-1058-4f9a-b0e0-e2ddf272f29c'
+        GIT_USERNAME_VARIABLE = 'miqbal@datainnovations.com'
+        GIT_PASSWORD_VARIABLE = 'Shinigami@456'
+        GIT_REPO_URL = 'https://github.com/iqbal-di-456/JenkinsTest.git'
+        DESTINATION_PATH = 'D:/D drive/Jenkins_File'
+    } else if (env.BRANCH_NAME == 'qa') {
         GIT_CREDENTIALS_ID = 'c17f17fc-1058-4f9a-b0e0-e2ddf272f29c'
         GIT_USERNAME_VARIABLE = 'miqbal@datainnovations.com'
         GIT_PASSWORD_VARIABLE = 'Shinigami@456'
         GIT_REPO_URL = 'https://github.com/iqbal-di-456/JenkinsTest.git'
         DESTINATION_PATH = 'D:/Test'
-    } else if (BRANCH_NAME == 'qa') {
-        GIT_CREDENTIALS_ID = 'c17f17fc-1058-4f9a-b0e0-e2ddf272f29c'
-        GIT_USERNAME_VARIABLE = 'miqbal@datainnovations.com'
-        GIT_PASSWORD_VARIABLE = 'Shinigami@456'
-        GIT_REPO_URL = 'https://github.com/iqbal-di-456/JenkinsTest.git'
-        DESTINATION_PATH = 'D:/D drive/Jenkins_Files'
     }
 
     // Return a map with the variables
