6975260cf2ff45ef7fd3462f57fe42ac2933e7c6
Md Iqbal <miqbal@datainnovations.com>
2023-12-19 22:49:03 +0530

    series env 11

---
+++ Fastfile
@@ -0,0 +1,14 @@
+require 'dotenv/load'
+
+
+default_platform(:ios)
+
+platform :ios do
+  lane : my_lane do
+    puts "Inside the lane: #{ENV["ENV"]}"
+  end
+
+#   lane :deploy do
+#     # Actions to deploy the iOS app
+#   end
+end
--- run-dev-script.js
+++ run-dev-script.js
@@ -1,5 +1,21 @@
 function myMethod() {
     console.log('Hello from DEV myMethod!');
+
+    const { exec } = require('child_process');
+
+    // Define your Fastlane command
+    const fastlaneCommand = 'fastlane my_lane';
+
+    // Execute the Fastlane command
+    exec(fastlaneCommand, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error executing Fastlane: ${error}`);
+        return;
+    }
+
+    console.log(`Fastlane output: ${stdout}`);
+    });
+
 }
 
 myMethod(); // This will call the method when the script is executed.
